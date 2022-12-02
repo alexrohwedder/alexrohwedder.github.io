b5e7f76b1e606f55582fffb0157fe9e93ef57e75
alexrohwedder <113746976+alexrohwedder@users.noreply.github.com>
2022-12-01 16:33:04 -0800

    Add files via upload

---
+++ proj4/classes.js
@@ -0,0 +1,215 @@
+class Portfolio {
+    constructor(offlineMode=false, arrData=false, maxMonths=60) {
+        this.arrData = arrData;
+        this.numStocks = 0;
+        this.maxMonths = this.findMaxMonths(maxMonths); // 60 is the standard 5 years, and is the maximum test period
+        this.nameArray = [];
+        if (offlineMode) {
+                // arrData represents the collection of all stored offline data
+                // This allows the portfolio to work even if the API functionality dies
+                for (let stock of arrData) {
+                    let name = stock["Meta Data"]["2. Symbol"];
+                    let prices = this.makePriceArray(stock);
+                    this[this.numStocks] = new Stock(name, prices, this.maxMonths);
+                    this.numStocks++;
+                    this.nameArray.push(name);
+                }
+        }
+        this.coVar = this.makeCovar();
+        this.muMatrix = this.makeMuMatrix()
+    }
+
+    findMaxMonths (initialMax) {
+        let maxMonths = initialMax;
+        for (let stock of this.arrData) {
+            let history = Object.keys(stock["Monthly Adjusted Time Series"]);
+            // adjust max months to minimum of available data
+            let len = history.length - 1;
+            maxMonths = maxMonths > len ? len : maxMonths;
+        }
+        return maxMonths;
+    }
+
+    makePriceArray(stock) {
+        let prices = [];
+        let history = Object.keys(stock["Monthly Adjusted Time Series"]);
+        history.slice(0, this.maxMonths + 1);
+        for (let time of history) {
+            prices.push(stock["Monthly Adjusted Time Series"][time]["5. adjusted close"]);
+        }
+        return prices;
+    }
+
+    makeCovar() {
+        let numStocks = this.numStocks;
+        // create a matrix object of the correct size
+        let vCv = new Matrix(arrOfZeros(numStocks, numStocks));
+        // for each stock pair (n^2) calculates the variance or covariance
+        for (let i = 0; i < numStocks; i++) {
+            for (let j = 0; j < numStocks; j++) {
+                vCv.matrixArray[j][i] = this.calcCovariance(this[i], this[j]);
+            }
+        }
+        return vCv;
+    }
+
+    calcCovariance(iStock, jStock) {
+        // Calculates the returns for two stocks in the portfolio
+        let iReturns = iStock.arrReturns;
+        let iAvg = iStock.mu;
+        let jReturns = jStock.arrReturns;
+        let jAvg = jStock.mu;
+
+        let sum = 0;
+        for (let t in iReturns) {
+            sum += (iReturns[t] - iAvg) * (jReturns[t] - jAvg);
+        }
+    return sum / (iReturns.length - 1);
+    }
+
+    makeMuMatrix() {
+        // Creates an array of stock returns
+        let muArray = [];
+        for (let ticker in this.nameArray) {
+            muArray.push([this[ticker].mu])
+        }
+        let muMatrix = new Matrix(muArray)
+        return muMatrix;
+    }
+
+    printHTMLtable() {
+        // Nicely formatted HTML string of matrix values
+        let coVarMatrix = this.coVar
+        let row = '<tr>'; 
+        for (let j = 0; j < coVarMatrix.rows + 1; j++) {
+            for (let i = 0; i < coVarMatrix.cols + 1; i++) {
+                if (i == 0 && j == 0) {
+                    row += '<td> ' + ' ' + '</td>';
+                } else if (i == 0) {
+                    row += '<th> ' + this.nameArray[j - 1] + ' </th>';
+                } else if (j == 0) {
+                    row += '<th> ' + this.nameArray[i -1] + ' </th>';
+                } else {
+                    row += '<td> ' + (coVarMatrix.matrixArray[j - 1][i - 1].toFixed(4)) + ' </td>';
+                }
+            }
+            row += '</tr>';
+        }
+        return `<table>${row}</table>`
+    }
+}
+
+class Stock {
+    // TODO: Average the monthly returns, then annualize them.
+    constructor(name, prices, months) {
+        this.ticker = name;
+        this.prices = prices;
+        this.numMonths = months;
+        this.arrReturns = this.makeRetunsArray(this.prices);
+        this.mu = this.calcAverage(this.arrReturns);
+    }
+
+    calcAverage(arrRetuns) {
+        let sum = arrRetuns.reduce((a, b) => a + b, 0);
+        return sum / this.numMonths;
+    }
+
+    makeRetunsArray(arrPrices) {
+        let arrReturns = [];
+        for (let i = 0; i < this.numMonths; i++) {
+            arrReturns.push(
+            this.calcReturn(arrPrices[i], arrPrices[i + 1])
+            );
+        }
+        return arrReturns;
+    }
+
+    calcReturn(a, b) {
+        let rate = (a - b) / b;
+        return rate;
+    }
+}
+
+class Matrix {
+    constructor(arrOfArrays) {
+    this.rows = arrOfArrays.length;
+    this.cols = arrOfArrays[0].length;
+    this.matrixArray = arrOfArrays;
+    }
+
+    dotProduct(bMatrix) {
+        // A * bMatrix 
+        // NOTE: MATRIX MULTIPLICATION IS NOT COMMUTATIVE 
+        // if multiplication is valid:
+        if (this.cols === bMatrix.rows) {
+            // make a new empty matrix of 0's:
+            let resMatrix = [];
+            for (let y = 0; y < this.rows; y++) {
+                let newRowArray = []
+                for (let x = 0; x < bMatrix.cols; x++) {
+                    newRowArray.push(0);
+                }
+                resMatrix.push(newRowArray);
+            }
+            resMatrix = new Matrix(resMatrix);
+            // Add values to the correct position in the resMatrix
+            for (let j = 0; j < resMatrix.rows; j++) {
+                for (let i = 0; i < resMatrix.cols; i++) {
+                    let sumProd = 0;
+                    for (let r = 0; r < this.cols; r++) {
+                        sumProd += this.matrixArray[j][r] * bMatrix.matrixArray[r][i];
+                    }
+                    resMatrix.matrixArray[j][i] = sumProd;
+                }
+            }
+                return resMatrix;
+            // If matrix multiplication can't work, return undefined.
+        } else {
+            return undefined;
+        }
+    }
+
+    transpose() {
+        // Matrix transpose
+        let newMatrixArray = [];
+        for (let i = 0; i < this.cols; i++) {
+            let newRowArray = [];
+            for (let j = 0; j < this.rows; j++) {
+                newRowArray.push(this.matrixArray[j][i]);
+            }
+            newMatrixArray.push(newRowArray);
+        }
+        let oldRows = this.rows;
+        this.rows = this.cols;
+        this.cols = oldRows;
+        this.matrixArray = newMatrixArray;
+    }
+
+    printMatrix() {
+        // Nicely formatted string of matrix values
+        // Primarily for debugging
+        var line = '';
+        for (let j = 0; j < this.rows; j++) {
+            for (let i = 0; i < this.cols; i++) {
+                line += this.matrixArray[j][i].toFixed(6) + ' ';
+                }
+            line += '\n';
+        }
+        return line;
+    }
+
+}
+
+function arrOfZeros(x, y, k=0) {
+    // creates an array y long of arrays x long
+    // TODO: Make this a matrix static method
+    let arrRes = [];
+    for (let j = 0; j < y; j++) {
+        let newRow = [];
+        for (let i = 0; i < x; i++) {
+            newRow.push(k);
+        }
+        arrRes.push(newRow);
+    }
+    return arrRes;
+}
---
+++ proj4/script.js
@@ -0,0 +1,250 @@
+MAX_STOCKS = 5; // Maximum stocks in a portfolio
+PORTFOLIO = undefined;
+BENCHMARK = undefined;
+PULL_ATTEMPT = false; // Flag to prevent multiple clicks on API button in pullButtonHandler
+COLLECTIONARRAY = [];
+
+window.addEventListener("DOMContentLoaded", initialLoadHander);
+
+function initialLoadHander() {
+    const apiButton = document.getElementById("api-button");
+    apiButton.addEventListener("click", apiButtonHandler)
+    
+    const changeAPIbutton = document.getElementById("change-api");
+    changeAPIbutton.addEventListener("click", changeAPI);
+
+    let userAPIkey = localStorage.getItem("alphaVantageAPIkey");
+    if (userAPIkey) {
+        clearScreen();
+    }
+
+    let ranges = document.querySelectorAll("input.range-stock");
+    for (let i = 0; i < MAX_STOCKS; i++) {
+        ranges[i].addEventListener("change", rangeStockHandler);
+    }
+
+    const pullButton = document.getElementById("get-data");
+    pullButton.addEventListener("click", pullButtonHandler);
+
+    const estimateButton = document.getElementById("port-weight");
+    estimateButton.addEventListener("click", estimateButtonHandler)
+    
+}
+
+function rangeStockHandler() {
+    // Get the values from the range element into an array
+    let valList = [];
+    let ranges = document.querySelectorAll("input.range-stock");
+    for (let i = 0; i < PORTFOLIO.numStocks; i++) {
+        valList.push(parseFloat(ranges[i].value));
+    }
+    
+    // Create the weighted portfolio array
+    let sum = valList.reduce((a, b) => parseFloat(a) + b, 0);
+    let weightedW = valList.map(w => w / sum);
+
+    // Add labels
+    let labels = document.getElementsByClassName("label-s-range");
+    for (let i = 0; i < PORTFOLIO.numStocks; i++) {
+        let val = weightedW[i];
+        if (isNaN(val)) {
+            val = 0;
+        }
+        labels[i].textContent = (val * 100).toFixed(2) + '%';
+    }
+}
+
+function clearScreen() {
+    document.getElementById("cover").style.display = "none";
+    document.getElementById("wrapper").style.display = "grid";
+}
+
+function changeAPI() {
+    document.getElementById("cover").style.display = "flex";
+    document.getElementById("wrapper").style.display = "none";    
+}
+
+function apiButtonHandler() {
+    const apiText = document.getElementById("api").value;
+    localStorage.setItem("alphaVantageAPIkey", apiText);
+    
+    clearScreen();
+}
+
+function pullButtonHandler() {
+    let selections = getInputTickers();
+    // Prevents mutliple calls of the API button 
+    if (!PULL_ATTEMPT) {
+        pullData(selections);
+        PULL_ATTEMPT = true;
+        setTimeout(resetPullAttempt, 2500);
+    }
+}
+
+function resetPullAttempt() {
+    PULL_ATTEMPT = false;
+}
+
+function getInputTickers() {
+    // called by the pullButtonHandler
+    let boxes = document.querySelectorAll("input.ticker-box");
+    let selections = [];
+    for (let box of boxes) {
+        if (box.value) {
+            selections.push(box.value.toUpperCase().trim());
+        }
+    }
+    return selections;
+}
+
+function pullData(selections) {
+    if (selections.length == 0) {
+        // If no data is given, the blankDataHandler is called to use offline data.
+        PORTFOLIO = blankDataHandler();
+    } else {
+        for (let ticker in selections) {
+            if (selections[ticker]) {
+                (requestTicker(selections[ticker]))
+            }
+        }
+        setTimeout(addDataToObject, 2000)
+    }
+}
+
+function addDataToObject() {
+    try {
+        PORTFOLIO = new Portfolio(true, COLLECTIONARRAY)
+        addDataToPage(PORTFOLIO.nameArray)
+    } catch {
+        blankDataHandler();
+    }
+}
+
+// Adapted from zyBooks
+async function requestTicker(ticker) {
+    //TODO: Add catch for bad or missing data
+    let apiKey = localStorage.getItem("alphaVantageAPIkey");
+    let requestURL = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${ticker}&apikey=${apiKey}`;
+    fetch(requestURL)
+        .then(response => {
+        console.log("status is " + response.status);
+            return response.text();
+        })
+        .then(html => COLLECTIONARRAY.push(JSON.parse(html)))
+        .catch(error => console.log("Request failed", error));
+}
+
+function blankDataHandler() {
+    alert(`
+Problem with data or insufficient data provided.
+Running model with saved historical data.
+Note: only 5 request a minute, 500 a day for each API key.
+    `)
+    // This saved data comes from the savedStocks file populated from previous API calls.
+    const savedData = [AAPL, JNJ, RTX, TRV, TXN];
+    PORTFOLIO = new Portfolio(true, savedData);
+    addDataToPage(PORTFOLIO.nameArray);
+    return PORTFOLIO;
+}
+
+function addDataToPage(portNameArray) {
+    // add range elements
+    for (let ticker in portNameArray) {
+        document.getElementById("range-d" + ticker).style.display = "grid";
+        document.getElementById("label-range-sn" + ticker).textContent = portNameArray[ticker];
+    } 
+    
+    // hide the pull div
+    const tickerPick = document.getElementById("stock-picker");
+    tickerPick.style.display = "none";
+
+    // unhide the weight div
+    const portDiv = document.getElementById("port-div");
+    portDiv.style.display = "flex";
+}
+
+function estimateButtonHandler() {
+    let omega = new Matrix(getWeights());
+    let mu = calcPortReturns(omega);
+    let sigma = calcPortVariance(omega); 
+    let sharpe = calcSharpeRatio(sigma, mu);
+
+    populateResults(omega, mu, sigma, sharpe);
+}
+
+function populateResults(omega, mu, sigma, sharpe) {
+    const displayDiv = document.getElementById("results");
+    displayDiv.style.display = "flex";
+
+    const pageLink = document.getElementById("refresh-page");
+    pageLink.style.display = "inline";
+
+    const muText = document.getElementById("mu-res");
+    muText.innerHTML = '&mu; = ' + toPercent(mu, 3);
+
+    const sigmaText = document.getElementById("si-res");
+    sigmaText.innerHTML = '&sigma; = ' + toPercent(sigma, 2);
+
+    const sharpeText = document.getElementById("sh-res");
+    sharpeText.innerHTML = 'Sharpe Ratio: ' + sharpe.toFixed(4);
+
+    const coVarText = document.getElementById("coVar");
+    coVarText.innerHTML = PORTFOLIO.printHTMLtable()
+
+    // SP500 Benchmark:
+    BENCHMARK = spyderDataHandler();
+    let spyderMu = BENCHMARK[0].mu;
+    const muBM = document.getElementById("mu-bm");
+    muBM.innerHTML = '&mu; = ' + toPercent(spyderMu, 3);
+
+    let spyderDev = BENCHMARK.coVar.matrixArray[0][0] ** 0.5;
+    const sigmaBM = document.getElementById("si-bm");
+    sigmaBM.innerHTML = '&sigma; = ' + toPercent(spyderDev, 2);
+
+    const sharpeBM = document.getElementById("sh-bm");
+    sharpeBM.innerHTML = 'Sharpe Ratio: ' + calcSharpeRatio(spyderDev, spyderMu).toFixed(4);
+}
+
+function toPercent(number, places = 2) {
+    return (number * 100).toFixed(places) + '%';
+}
+
+function getWeights() {
+    // Get the values from the range element into an array
+    let valList = [];
+    let ranges = document.querySelectorAll("input.range-stock");
+    for (let i = 0; i < PORTFOLIO.numStocks; i++) {
+        valList.push(parseFloat(ranges[i].value));
+    }
+    let sum = valList.reduce((a, b) => parseFloat(a) + b, 0);
+    // If you zero all the portfolio values, you'll get zero, not NaN
+    let weightedW = valList.map(w => sum == 0 ? 0 : w / sum);
+    let omega = [];
+    omega.push(weightedW)
+    return omega;
+}
+
+function calcPortReturns(omega) {
+    let muPort = omega.dotProduct(PORTFOLIO.muMatrix)
+    // dot product will reduce the matrix to one element
+    return muPort.matrixArray[0][0];
+}
+
+function calcPortVariance(omega) {
+    // w * S * w' 
+    let firstHalf = omega.dotProduct(PORTFOLIO.coVar);
+    omega.transpose();
+    let sigmaPort = firstHalf.dotProduct(omega);
+    omega.transpose();
+    return sigmaPort.matrixArray[0][0] ** 0.5;
+}
+
+function calcSharpeRatio(sigma, mu) {
+    // Assuming that risk-free rate is zero, because I'm not calculating it
+    return mu / sigma;
+}
+
+function spyderDataHandler() {
+    let spy = new Portfolio(true, [SPY], PORTFOLIO.maxMonths);
+    return spy;
+}
